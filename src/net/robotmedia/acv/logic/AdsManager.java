02f830bbd7b092e983f7f6dcc7c27ba5f8b69bdd
Hermes Pique <hermes.pique@robotmedia.net>
2012-07-25 11:05:53 +0200

    Remove AdMobProxy

--- src/net/robotmedia/acv/logic/AdsManager.java
+++ src/net/robotmedia/acv/logic/AdsManager.java
@@ -1,10 +1,15 @@
 package net.robotmedia.acv.logic;
 
+import com.google.ads.AdRequest;
+import com.google.ads.AdSize;
+import com.google.ads.AdView;
+
+import net.androidcomics.acv.BuildConfig;
 import net.androidcomics.acv.R;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
-import android.view.View;
+import android.content.res.TypedArray;
 
 public class AdsManager {
 
@@ -21,16 +26,40 @@ public class AdsManager {
 	// (equivalent to Configuration.SCREENLAYOUT_SIZE_XLARGE, but avoiding reflection for this)
 	protected static final int RETRO_SCREENLAYOUT_SIZE_XLARGE = 4;
 	
-	public static View getAd(Activity activity, int size) {
+	public static AdView getAd(Activity activity, int size) {
 		init(activity);
 		if (!usesAds) return null;
 
-		View ad = AdMobProxy.getAd(activity, size, publisherId, testDeviceId);
-		ad.setId(R.id.ad);
-		return ad;
+		AdView adView = new AdView(activity, sizeToAdSize(size), publisherId);
+		AdRequest request = new AdRequest();
+		
+		setExtras(request, activity);
+		
+		if (BuildConfig.DEBUG && testDeviceId != null) {
+			request.addTestDevice(testDeviceId);
+			request.addTestDevice(AdRequest.TEST_EMULATOR);
+		}
+		adView.loadAd(request);		
+		adView.setId(R.id.ad);
+		return adView;
 	}
 	
-	public static View getAd(Activity activity) {
+	private static void setExtras(AdRequest request, Activity activity) {
+    	final int[] attrs = new int[] { R.attr.acv__primary_color};
+    	TypedArray values = activity.getTheme().obtainStyledAttributes(attrs);
+    	final int primaryColor = values.getColor(0, activity.getResources().getColor(R.color.acv__green));
+    	values.recycle();
+		final String primaryColorString = String.format("#%06X", (0xFFFFFF & primaryColor));
+
+		request.addExtra("color_bg", primaryColorString);
+		request.addExtra("color_bg_top", primaryColorString);
+		request.addExtra("color_border", primaryColorString);
+		request.addExtra("color_link", primaryColorString);
+		request.addExtra("color_text", primaryColorString);
+		request.addExtra("color_url", primaryColorString);
+	}
+	
+	public static AdView getAd(Activity activity) {
 		init(activity);
 		if (!usesAds) return null;
 		
@@ -46,9 +75,13 @@ public class AdsManager {
 	
 	public static void destroyAds(Activity activity) {
 		if (!usesAds) return;
-		
-		AdMobProxy.destroyAds(activity, R.id.ad);
+
+		AdView adView = (AdView) activity.findViewById(R.id.ad);
+		if (adView != null) {
+			adView.destroy();
+		}
 	}
+	
 
 	protected static void init(Context context) {
 		if (!usesAds) return;
@@ -73,4 +106,14 @@ public class AdsManager {
 	public static void disableAds() {
 		usesAds = false;
 	}
+	
+	private static AdSize sizeToAdSize(int size) {
+		switch(size) {
+			case AdsManager.SIZE_FULL_BANNER:
+				return AdSize.IAB_BANNER;
+			default:
+				case AdsManager.SIZE_BANNER:
+				return AdSize.BANNER;	
+		}
+}
 }
